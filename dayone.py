632523b7c5b296416fac1210b0dbd4f99b956a60
TavkirS <taukirofficial15@gmail.com>
2025-12-05 23:17:52 +0530

    first commit

---
+++ dayone.py
@@ -0,0 +1,77 @@
+# What is python
+# Python is a high-level, easy-to-learn, powerful programming language used for web development, automation, data science, AI, and more.
+# ===============================
+
+# Print Statement
+# print("Helloo World")
+
+# ===============================
+
+#  Variables
+# -----------
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+
+name = "Sarthak" 
+
+# ===============================
+
+# Naming Rules for Python variables
+# -------------------------
+# myname
+# my_nam
+# myName => camel case
+# MyName1 => pascal case
+# my_name_one  => snake 
+
+# ===============================
+
+# Many Values to Multiple Variables
+# ---------------------------------
+# name1, name2, name3 = "Kunali", "Sarthak" , "Taukir"
+# print(name1)
+# name1, name2, name3 = "Sarthak", "Rajesh" , "Taukir"
+# # name1 = name2 = name3 = "Sarthak"
+# print(name1)
+
+# ===============================
+
+# One Value to Multiple Variables
+# ---------------------------------
+# x = y = z = "Orange"
+# print(x)
+
+# ===============================
+
+# In the print() function, you output multiple variables, separated by a comma:
+# You can also use the + operator to output multiple variables:
+# (+ , ",")
+
+# name = "Sarthak"
+# age = "is"
+# roll = "Engg"
+
+# print(name,age,roll)
+# print(name+age+roll)
+
+# ===============================
+
+# Global Variable
+# ---------------
+# Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+# Global variables can be used by everyone, both inside of functions and outside.
+
+# x= "Sarthak is awsome"
+
+# def myfun():
+#     global x
+#     x="Kunali is awsome"
+
+# myfun()
+# print(x)
+# print(x) 
+
+# We canot give a name to variable which alredy built in python 
+
+# ===============================
+
---
+++ daytwo.py
@@ -0,0 +1,117 @@
+# Data Types
+# -------------
+# Variables can store data of different types, and different types can do different things.
+
+# Built-in Data Types
+# -----------------
+# Text Type:	str
+# Numeric Types:	int , float , complex 
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set,
+# Boolean Type:	bool
+ 
+
+# Python Numbers
+# -----------------
+# There are three numeric types in Python:
+# int  15
+# float  25.5
+# complex  6 + 5j
+
+# complex number = real part + imaginary part
+
+# Python Casting
+# -----------------
+# x = int(1)   
+# y = int(2.8) 
+# z = int("3") 
+
+# ============
+
+# Name = "Name"
+# age = 18
+# weight = 50.22
+# adress = "at post deoli"
+# contact = 8888234984
+# isstudentsmoke = False
+# percentage = 98.88
+# studentmigrationadress =  None 
+ 
+
+# print(type(Name))
+# print(type(age))
+# print(type(weight))
+# print(type(isstudentsmoke))
+# print(type(studentmigrationadress))
+
+
+# =============================
+
+# type() use to check the datatype
+
+student = ["Sarthak","Kunali","Sumer"] # Square bracket list mutuable
+
+# print(student)
+# print(type(student))
+# print(student[0])
+# print(student[-3])
+
+student=("Sarthak","Kunali","Sumer") # Round Braces tuple unmutable
+# print(student)
+# print(type(student))
+# print(student[1])
+
+# in py there is built it function to check the data type
+
+# range(start,stop,step)
+
+
+# for i in range(5):
+#     print("Happy birthdy Mmy")
+
+
+# in dict we have the pairs of keys values
+
+# classstu = {"name":"sumer","age":18,"city":"Deoli"}
+# print(classstu)
+# print(type(classstu))
+
+
+# set is unordered collection of unique data 
+
+# student={"S","K","T"}
+# print(student)
+# print(type(student))
+
+
+x = int(2.1) 
+# print(x) 
+# print(type(x))
+
+weight =float(60)
+# print(weight)
+# print(type(weight))
+
+age=str(18)
+# print(age)
+# print(type(age))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
