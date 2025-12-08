b93a832b798c3846aa0e494637d9d2800d4505eb
TavkirS <taukirofficial15@gmail.com>
2025-12-09 00:00:46 +0530

    Operators

---
+++ dayFour.py
@@ -0,0 +1,95 @@
+# Operator 
+# -----------------
+
+# operator is a symbols that perform an action on values/variables in a program
+
+# 1) Arithmatic Operator 
+# 2) Assignment Operator
+# 3) Comparison Operator
+# 4) Logical Operator
+# 5) Identity Operator
+# 6) Membership Operator
+
+# 1) Arithmatic Operator 
+# ----------------------------
+
+# + , - , * , / , // , % , **
+
+# addition = 2+2  #Operands
+# print(addition)
+# Sub = 2-2 
+# print(Sub)
+# multi = 2*2 
+# print(multi)
+# div = 10/4  
+# print(div)
+# floordiv = 10 // 4
+# print(floordiv)
+# modulus = 10 % 4
+# print(modulus)
+# exponent =2 ** 3
+# print(exponent)
+
+# 2) Assignment Operator
+# ----------------------------
+
+# = , += , -= , *= , /= , //= , %=  , **=
+
+# val = 5
+# val = 5+5 
+# print(val)
+
+# multi = 6
+# multi * 2 
+# multi *= 2
+# print(multi)
+
+# x= 2
+# x **= 3
+# print(x)
+
+
+# 3) Comparison Operator
+# ------------------------
+
+# == , != , > , >=, < ,<=
+
+# 4) Logical Operator
+ 
+# and , or , not 
+
+# 5) Identity Operator
+
+# is , not 
+
+# a= 6
+# b=a
+
+# print(a is  b)
+
+
+# 6) Membership Operator
+# --------------------------
+# in not in 
+
+# am= [1,2,3]
+# print(1 in am)
+# print(5 not in am)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ daythree.py
@@ -0,0 +1,23 @@
+# Python Input
+# -----------------
+
+# name=input("Enter Your Name")
+# print(name)
+# print(type(name))
+
+# age=int(input("Enter Your Age"))
+# print(age)
+# print(type(age))
+# print(age+10)
+
+# weight=float(input("Enter your weight"))
+# totalweight = weight + 6
+# print(totalweight)
+
+
+
+
+
+
+
+
