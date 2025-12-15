91989f6237e4a160a94b090f7c5aef6c4fa0e01c
TavkirS <taukirofficial15@gmail.com>
2025-12-15 23:03:14 +0530

    Day09 string

---
+++ day9.py
@@ -0,0 +1,44 @@
+# String
+# String is a collection of charaters  
+
+# we can give in string number(123), Symbol(!@%#&!*), letter(abcs)
+# we can write the the string in either ex. "" or ''
+# Immutable string change nhi hoti
+# left indexing start from 0 
+# right indexing starts from -1
+
+name="aman"
+name2="TAUKIR"
+msg="Wellcome to 9 th @string chapter"
+
+# String Operations
+# ====================
+
+# len()
+lenofname = len(name)
+# print(lenofname)
+
+# Indexing()
+# print(name[1])
+# print(name[-1])
+
+# Slicing [startswith:endswith] endswith excluded
+# print(msg[0:-1])
+
+# Upper()
+print(name.upper())
+
+# Lower()
+print(name2.lower())
+
+
+
+
+
+
+
+
+
+
+
+
