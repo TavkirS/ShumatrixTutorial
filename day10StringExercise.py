28a6270939c007bd8af857ce3cc4c3f0e91a3c87
TavkirS <taukirofficial15@gmail.com>
2025-12-22 23:15:40 +0530

    For Loop

---
+++ day10StringExercise.py
@@ -0,0 +1,59 @@
+# Email Validator
+
+# Logic 
+# --------
+# @ hona chahiye
+# . hona chahiye
+# space nahi hona chahiye
+
+# email = input("Enter your Email\n")
+
+# if "@" in email and "." in email and " " not in email and "gmail.com"or"yahho.com" in email:
+#     print("Email is correct")
+# else:
+#     print("Invalid Email")
+
+# Text Analyser
+
+# message =input("Enter your Tweet\n")
+# words=message.split()
+
+# print("Words",len(words))
+# print("Character",len(message))
+# print("Spaces",message.count(" "))
+# print("Fullstop",message.count("."))
+# print("Hash",message.count("#"))
+
+# String Operation
+
+# message = input("Enter your message\n")
+
+# print("Character",len(message))
+# print(message.strip())
+# print(message.upper())
+# print(message.lower())
+
+
+
+# Day one to Day 10 go through 
+# Brushup operator and its types 
+# List methods brushup
+# Dict practice with solution
+# String ko padhna hai all methods
+# String ki exercise karna hai 
+# String exercise code ko scale karna hai
+# till monday EOD
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day11.py
@@ -0,0 +1,65 @@
+# For loop 
+# for loop ka use ek kaam ko baar baar repeat karne ke liye hota hai 
+# Coding Principle --> DRY = Do Not Repeat Youself
+
+# Syntax 
+# for variable in sequence:
+#     statement
+
+# iterable jisko loop me chala sakte hai
+
+# ✅ Iterable ex:
+# String
+# list
+# Tuple
+# range
+# Dict
+
+# ❌ Non Iterable:
+# int
+# Float
+
+
+# range() number ki sequence ko banata hai
+# print(range(500,1000))
+
+num = 50
+
+# Loop on message
+# for i in range(51):
+#     print(i,"Sumer is great")
+
+# for loop on list
+# fruits = ["Banana","Mango","Apple","Grapes"]
+# for i in fruits:
+#     print(i)
+
+# for loop on String
+# str = "Python"
+# for i in str:
+#     print(str)
+
+
+# for loop with condition (if)
+
+# for i in range(1,11):
+#     if i % 2 == 0:
+#         print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
