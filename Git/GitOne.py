dfc33b983ced1b01fc5ce3814bf09d4a3cae86ff
TavkirS <taukirofficial15@gmail.com>
2026-03-13 22:54:15 +0530

    Git Day 1

---
+++ Git/GitOne.py
@@ -0,0 +1,70 @@
+# Class 1: Version Control & Git Introduction
+# What is Version Control?
+# Version Control is a system that helps developers track 
+# and manage changes in their code over time. It allows multiple versions 
+# of a project to be stored, compared, and restored whenever needed.
+
+# Why Version Control is Important?
+# Version Control is important because it helps developers manage code changes, 
+# collaborate with team members, and maintain a complete history of a project.
+
+# Problems Without Version Control
+# Without Version Control, developers may lose important code changes, 
+# face difficulty in collaboration, and struggle to manage multiple versions of 
+# the same project.
+
+# What is Git?
+# Git is a distributed version control system that helps developers track changes 
+# in their code and collaborate with others efficiently.
+
+
+# What is GitHub?
+# GitHub is a cloud-based platform that hosts Git repositories and allows developers to collaborate, share, and manage their code online.
+
+# Git vs GitHub
+# | Git                             | GitHub                          |
+# | ------------------------------- | ------------------------------- |
+# | Version Control Tool            | Online Platform                 |
+    # Language
+# | Local system par kaam karta hai | Cloud par code store karta hai  |
+# | Code history manage karta hai   | Collaboration provide karta hai |
+
+# What is Git Installation?
+# Git installation is the process of downloading and setting 
+# up Git on a computer so that developers can use Git commands to 
+# manage their projects.
+
+# Installing Git (Windows)
+# Steps
+
+# Website:
+# https://git-scm.com
+
+# Check Git Installation
+# git --version
+
+# What is Git Configuration?
+# Git configuration is the process of setting up user 
+# information such as name and email so that Git can identify who made changes to
+# the project.
+
+# Git Global Configuration
+# Global configuration sets the default username and email for 
+# all Git repositories on a system.
+    
+# Commands
+# Set Username
+# git config --global user.name "Manju"
+
+# Set Email
+# git config --global user.email "Teju@email.com"
+
+# Verify Configuration
+# git config --list
+
+# What is Git Bash?
+# Git Bash is a command-line interface that allows users to run Git 
+# commands on Windows systems.
+# git init
+# git add
+# git commit
